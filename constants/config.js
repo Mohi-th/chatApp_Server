c202d201775fce0794feb16978bb9d91f4a49e23
Mohi-th <mohithmohith880@gmail.com>
2024-11-29 18:45:33 +0530

    client url updated

--- constants/config.js
+++ constants/config.js
@@ -1,6 +1,8 @@
 const corsOptions = {
   origin: [
     "http://localhost:5173",
+    "http://localhost:5174",
+    "https://chat-app-frontend-ro67iluq0-mohiths-projects-12313acc.vercel.app",
     "http://localhost:4173",
     process.env.CLIENT_URL,
   ],
