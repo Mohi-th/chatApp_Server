bf6709028d2266a7ecdb7a3035a5a10e571bb635
Mohi-th <mohithmohith880@gmail.com>
2024-12-01 22:58:25 +0530

    new origin added

--- constants/config.js
+++ constants/config.js
@@ -2,6 +2,7 @@ const corsOptions = {
   origin: [
     "http://localhost:5173",
     "http://localhost:5174",
+    "https://chat-app-frontend-4lovfnqow-mohiths-projects-12313acc.vercel.app",
     "https://chat-app-frontend-96acupq5p-mohiths-projects-12313acc.vercel.app",
     "https://chat-app-frontend-ro67iluq0-mohiths-projects-12313acc.vercel.app",
     "https://chat-app-frontend-6onjzxpo2-mohiths-projects-12313acc.vercel.app",
